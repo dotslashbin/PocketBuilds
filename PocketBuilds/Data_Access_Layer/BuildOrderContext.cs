05f66a0e142bab4b2c4644cb3872bd4d1bf69511
dotslashbin <joshuarpf@gmail.com>
2014-01-31 13:03:27 +0800

    Added initial BuildOrderContext and initializer

---
+++ PocketBuilds/Data_Access_Layer/BuildOrderContext.cs
@@ -0,0 +1,25 @@
+﻿using PocketBuilds.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace PocketBuilds.Data_Access_Layer
+{
+    public class BuildOrderContext : DbContext
+    {
+        public BuildOrderContext() : base( "BuildOrderContext" )
+        {
+        }
+
+        public DbSet<BuildOrder> BuildOrders { get; set; }
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ PocketBuilds/Data_Access_Layer/BuildOrderInitializer.cs
@@ -0,0 +1,30 @@
+﻿using PocketBuilds.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PocketBuilds.Data_Access_Layer
+{
+    public class BuildOrderInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<BuildOrderContext>
+    {
+        protected override void Seed(BuildOrderContext context)
+        {
+            var BuildOrders = new List<BuildOrder>
+            {
+                new BuildOrder{ Title = "3 GATE ROBO", Content = "This is the first test" }, 
+                new BuildOrder{ Title = "TWILIGH ALL-IN", Content = "This is the second test" }
+            };
+            BuildOrders.ForEach(build => context.BuildOrders.Add(build)); 
+            context.SaveChanges();
+
+            var Notes = new List<Note>
+            {
+                new Note{ BuildOrderID=1, Content = "the first note" },
+                new Note{ BuildOrderID=2, Content = "The second note" }
+            }; 
+            Notes.ForEach(note => context.Notes.Add(note));
+            context.SaveChanges(); 
+        }
+    }
+}
