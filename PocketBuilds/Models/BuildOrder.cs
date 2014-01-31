d6234414118f9dc1eb9cec1592591cf3b37ff681
dotslashbin <joshuarpf@gmail.com>
2014-01-31 12:54:26 +0800

    Added initial definitions of BuildOrder and Note models

---
+++ PocketBuilds/Models/BuildOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PocketBuilds.Models
+{
+    public class BuildOrder
+    {
+        public int ID { get; set; }
+        public string title { get; set; }
+        public string Content { get; set; }
+
+        public virtual ICollection<Note> Notes { get; set; }
+    }
+}
---
+++ PocketBuilds/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PocketBuilds.Models
+{
+    public class Note
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+    }
+}
