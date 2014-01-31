8e5ef9d48fe19459982eb2e34eba4751e79cba09
dotslashbin <joshuarpf@gmail.com>
2014-01-31 12:37:16 +0800

    Added initial project files

---
+++ PocketBuilds/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PocketBuilds.MvcApplication" Language="C#" %>
---
+++ PocketBuilds/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PocketBuilds.Startup))]
+namespace PocketBuilds
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
