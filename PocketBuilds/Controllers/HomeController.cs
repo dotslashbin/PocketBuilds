2bd6d01a849ae2b7fefb5d7d75d7c01909bf4b7a
dotslashbin <joshuarpf@gmail.com>
2014-02-16 21:56:58 +0800

    Code cleanup

--- PocketBuilds/Controllers/HomeController.cs
+++ PocketBuilds/Controllers/HomeController.cs
@@ -22,14 +22,12 @@ namespace PocketBuilds.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
